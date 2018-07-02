1a1048d1a6caf8721edec9a52d438bbf593c1c07
Karri Aravind Reddy <karriaravind@gmail.com>
2018-07-02 23:07:37 +0200

    changing every ting
    large makeover

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#typed", {
+  strings: ["Hello ", "I am: Aravind","I am: <i>A designer</i>", "I am: <strong>A Developer</strong>","My website is under construction"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 500,
+    startDelay: 1000,
+    smartBackspace: true,
+    loop: true,
+    cursorChar: ''
+});
