c97b573b95a6df33d8b85523fad93c6513210986
Karri Aravind Reddy <karriaravind@gmail.com>
2018-07-03 21:19:02 +0200

    Add files via upload

---
+++ profile/js/script.js
@@ -0,0 +1,11 @@
+ var rellax = new Rellax('.rellax', {
+            // center: true
+            callback: function(position) {
+                // callback every position change
+                console.log(position);
+            } var rellax = new Rellax('.rellax', {
+            // center: true
+            callback: function(position) {
+                // callback every position change
+                console.log(position);
+            }
